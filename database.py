4d1ed2a40cd2886a188dec4394665b72e60c8653
KhanSimak <mdsimak31@gmail.com>
2026-02-28 03:23:46 +0530

    Removed dotenv for production

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL="postgresql://postgres:simak007@localhost:5432/clinicdb"
-engine=create_engine(DATABASE_URL)
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-SessionLocal=sessionmaker(
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-Base=declarative_base()
+
+Base = declarative_base()
