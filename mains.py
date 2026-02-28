a8196ef30a2796597592c4f47548de8016ccd9f9
KhanSimak <mdsimak31@gmail.com>
2026-02-28 16:15:16 +0530

    updated mains

--- mains.py
+++ mains.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,Request
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from database import engine, SessionLocal
