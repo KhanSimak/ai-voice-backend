6ba699a939c3584a8c4ae9075b9b37dda59b237e
KhanSimak <mdsimak31@gmail.com>
2026-03-31 04:10:03 +0530

    fixed env conflict

--- database.py
+++ database.py
@@ -2,6 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+# ONLY load .env locally (not on Render)
+if os.getenv("RENDER") is None:
+    from dotenv import load_dotenv
+    load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
@@ -10,12 +15,9 @@ if not DATABASE_URL:
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-print("Using DB:", DATABASE_URL)  # debug
+print("Using DB:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL not set")
