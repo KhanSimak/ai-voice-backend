71353d7434473fa81226838510b28b5b06b7247a
KhanSimak <mdsimak31@gmail.com>
2026-03-31 04:29:48 +0530

    force fix database

--- database.py
+++ database.py
@@ -1,9 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-
-print("🔥 ALL ENV VARS:", dict(os.environ))   # shows everything
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-print("🔥 DATABASE_URL VALUE:", DATABASE_URL)
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL missing")
 
-raise Exception("STOP HERE")  # stop app intentionally
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+print("✅ USING DB:", DATABASE_URL)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
