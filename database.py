7be8d09cf3ea1bdaf13bc7aff1b96a865a6a0a2f
KhanSimak <mdsimak31@gmail.com>
2026-03-31 03:49:50 +0530

    requirements

--- database.py
+++ database.py
@@ -2,17 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set")
+    raise ValueError("DATABASE_URL not set")
 
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+print("Using DB:", DATABASE_URL)  # debug
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set")
